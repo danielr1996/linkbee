785b2daca61d50e24ac0ce0240d9f7a0bd199081
Daniel Richter <danielrichter@posteo.de>
2023-04-29 01:37:09 +0200

    feat: implement parent helm chart and multiarch docker build
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- pushdocker.sh
+++
@@ -1,4 +0,0 @@
-docker build -t ghcr.io/danielr1996/linkbee-frontend ./frontend/
-docker push ghcr.io/danielr1996/linkbee-frontend
-docker build -t ghcr.io/danielr1996/linkbee-backend ./backend/
-docker push ghcr.io/danielr1996/linkbee-backend
