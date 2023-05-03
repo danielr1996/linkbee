541693c630d632f55b496ba16301a38d7801252a
Daniel Richter <danielrichter@posteo.de>
2023-05-03 07:04:36 +0200

    feat: add build script
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- ci.sh
+++ ci.sh
@@ -1,3 +1,2 @@
-VERSION=3
-docker buildx build --push --platform=linux/arm64,linux/amd64 -t ghcr.io/danielr1996/linkbee-frontend:"$VERSION" ./frontend/
-docker buildx build --push --platform=linux/arm64,linux/amd64 -t ghcr.io/danielr1996/linkbee-backend:$VERSION ./backend/
+ci/setversion.sh
+ci/build.sh
